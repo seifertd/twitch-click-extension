be8b7b59cec541bb64e07b63e32c60894e65fd5c
Douglas A. Seifert <doug@uptake.com>
2021-04-26 16:42:33 -0700

    Manage on/off state per tab

--- harvest.js
+++ harvest.js
@@ -2,10 +2,8 @@ function harvest() {
   const foo = () => {
     const myButton = document.querySelector("button.tw-button.tw-button--success");
     if (myButton) {
-      console.log("Clicking the button, yo!");
       myButton.click();
     }
-    console.log("Resetting auto click handler ...");
     window.harvestTimeout = window.setTimeout(foo, 60000);
   };
   foo();
@@ -18,30 +16,69 @@ function deharvest() {
   }
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("I AM RUNNING!");
-  chrome.storage.sync.set({ drlupo: 'off' });
-});
-chrome.action.onClicked.addListener( async (tab) =>{
-  console.log("Extension was clicked with tab: ", tab);
-  if ( tab.url === 'https://www.twitch.tv/DrLupo' ) {
-    chrome.storage.sync.get("drlupo", ({ drlupo }) => {
-      console.log("GOT STATE: ", drlupo);
-      if ( drlupo === 'off' ) {
-        chrome.storage.sync.set({ drlupo: 'on' });
-        chrome.action.setBadgeText( { tabId: tab.id, text: 'ON' });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: harvest
-        });
-      } else {
-        chrome.storage.sync.set({ drlupo: 'off' });
-        chrome.action.setBadgeText( { tabId: tab.id, text: 'OFF' });
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: deharvest
-        });
-      }
+function readLocalStorage(key) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], (result) => {
+      //console.log(`LOCAL STORAGE GET OF KEY ${key}: `, result);
+      resolve(result);
     });
+  });
+}
+
+function writeLocalStorage(value) {
+  //console.log(`WRITING TO LOCAL STORAGE: `, value);
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.set(value, () => {
+      resolve();
+    });
+  });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  //console.log("I AM RUNNING!");
+  chrome.storage.local.set({ drlupo: {} });
+});
+
+chrome.tabs.onRemoved.addListener( async ( tabId, removeInfo ) => {
+   const tabConfig = await readLocalStorage("drlupo");
+   const { drlupo } = tabConfig;
+   //console.log(`TAB IS GOING AWAY: ${tabId}`, tabConfig, drlupo);
+   if ( drlupo && drlupo[tabId] ) {
+     delete drlupo[tabId];
+     writeLocalStorage(tabConfig);
+   }
+});
+
+chrome.action.onClicked.addListener( async (tab) =>{
+  //console.log("GOT CLICK FOR TAB ", tab);
+  if ( tab.url === 'https://www.twitch.tv/DrLupo' ) {
+    const storageKey = `drlupo`;
+    const tabConfig = await readLocalStorage(storageKey);
+    const { drlupo } = tabConfig;
+    //console.log("READ STORAGE: ", tabConfig, drlupo);
+    if ( !drlupo[tab.id] ) {
+      // initialize storage
+      drlupo[tab.id] = 'off';
+      await writeLocalStorage(tabConfig);
+    }
+    const tabState = drlupo[tab.id];
+    //console.log("   --> DRLUPO!: ", tabState);
+    if ( tabState === 'off' ) {
+      drlupo[tab.id] = 'on';
+      await writeLocalStorage(tabConfig);
+      chrome.action.setBadgeText( { tabId: tab.id, text: 'ON' });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: harvest
+      });
+    } else {
+      drlupo[tab.id] = 'off';
+      await writeLocalStorage(tabConfig);
+      chrome.action.setBadgeText( { tabId: tab.id, text: 'OFF' });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: deharvest
+      });
+    }
   }
 });
